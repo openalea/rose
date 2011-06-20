5d7f5f6873345b08ad3d9ce0c2a8136d0ba4e709
Christophe Pradal <christophe.pradal@inria.fr>
2011-06-20 08:22:04 +0000

    turtle interpretation
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@1383 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ rose/rose/turtle.py
@@ -0,0 +1,56 @@
+import openalea.plantgl.all as pgl
+from openalea.mtg.turtle import traverse_with_turtle
+
+radius =1.2 
+def compute_leaf(points):
+    # replace this function with your own one.
+    barycenter = sum(points, pgl.Vector3())/len(points)
+    distance = barycenter-points[0]
+    radius = pgl.norm(distance)/10.
+    return pgl.Translated(distance, pgl.Sphere(radius))
+    
+def visitor(g, v, turtle, leaf_factory=compute_leaf):
+    n = g.node(v)
+    pt = position(n)
+
+    symbol = n.label[0]
+    if symbol in ['E', 'R']:
+        if n.edge_type() == '+' :
+            turtle.startGC()
+        turtle.setId(v)
+        turtle.lineTo(pt)
+    if n.label =='F1':
+        turtle.setId(v)
+        turtle.incColor()
+        points = [position(n.parent()), pt]
+        while n.nb_children() == 1:
+            n = list(n.children())[0]
+            points.append(position(n))
+        turtle.startPolygon()
+        for pt in points[1:]:
+            turtle.lineTo(pt)
+        turtle.lineTo(points[0])
+        turtle.stopPolygon()
+        # set the shape to the turtle
+        #geom = leaf_factory(points)
+        #turtle.customGeometry(geom, 1)
+        
+
+
+def position(n):
+    return pgl.Vector3(n.XX, n.YY, n.ZZ)
+
+def TurtleFrame(g, visitor=visitor):
+    n = g.max_scale()
+    turtle = pgl.PglTurtle()
+    turtle.setWidth(radius)
+    for plant_id in g.vertices(scale=1):
+        plant_node = g.node(plant_id)
+        origin = position(plant_node)
+        turtle.move(origin)
+
+        vid =  g.component_roots_at_scale(plant_id, scale=n).next()
+        traverse_with_turtle(g, vid, visitor, turtle)
+    return turtle.getScene()
+
+
