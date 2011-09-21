abd8e1dc8da6e14cafb1bda6bf46abd9852158fe
Jessica Bertheloot <jessica.bertheloot@angers.inra.fr>
2011-09-21 09:42:26 +0000

    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@1525 0978ab36-7443-0410-8248-c2aa344a765a

--- rose/rose/GetLocalFiles.py
+++ rose/rose/GetLocalFiles.py
@@ -1,10 +1,12 @@
 import os
 
-def GetLocalFiles(filesInPath):
+def GetLocalFiles(filesInPath=[]):
     '''    Create a dictionnary from the list of available files 
     '''
     dictoffilenames = {}; 
-    # write the node code here.
+    # write the node code here.
+    #filesInPath=filesInPath.split("\n")
+    filesInPath=(filesInPath)
     for fichier in filesInPath:
         nomFich = os.path.basename(fichier)
         dictoffilenames[nomFich] = fichier
