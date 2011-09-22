b4de8be124756ce2d4013df23de2a3b5035e828d
Hervé Autret <Herve.Autret@angers.inra.fr>
2011-09-22 15:43:45 +0000

    added up a composite node 'Canopy2011Driver' to drive the directory names according to the position file in use. This runs together with the python script rose/makeFakeMissingGrids.py, that copies the position files into the right directories (and makes fake position files when real ones are missing, for testing purposes). makeFakeMissingGrids.py is to be ran only once, when all position files (e.g 2011-M1-PARfort-P1.txt) are available in the ./rose directory.
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@1540 0978ab36-7443-0410-8248-c2aa344a765a

--- rose/rose/GetLocalFiles.py
+++ rose/rose/GetLocalFiles.py
@@ -1,14 +1,14 @@
-import os
-
+import os
+
 def GetLocalFiles(filesInPath=[]):
     '''    Create a dictionnary from the list of available files 
     '''
     dictoffilenames = {}; 
-    # write the node code here.
-    #filesInPath=filesInPath.split("\n")
+    # write the node code here.
+    #filesInPath=filesInPath.split("\n")
     filesInPath=(filesInPath)
-    for fichier in filesInPath:
-        nomFich = os.path.basename(fichier)
+    for fichier in filesInPath:
+        nomFich = os.path.basename(fichier)
         dictoffilenames[nomFich] = fichier
     # return outputs
     return dictoffilenames,
