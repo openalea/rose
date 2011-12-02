5c16af071af643a3c7515b8b89126e5eae66569a
Hervé Autret <Herve.Autret@angers.inra.fr>
2011-12-02 10:29:50 +0000

    svn added the python files
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@1715 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ rose/rose/MyPackageDir.py
@@ -0,0 +1,8 @@
+def MyPackageDir(name):
+    '''    gets the actual path of the package, even in windows
+    '''
+    packagedir = None; 
+    # write the node code here.
+
+    # return outputs
+    return packagedir,
